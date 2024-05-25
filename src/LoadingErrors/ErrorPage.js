3e3b028f5a4de3a37b3fb15241e0b00b1b76df66
Aristeidis Triantafyllidis <tria.aris@gmail.com>
2024-05-25 16:25:26 +0300

    Added catch error in Api calls

---
+++ src/LoadingErrors/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ErrorPage(){
+
+    return(
+
+        <div className="error">
+            <h1>Something went wrong with the page</h1>
+            <img src="./warning.png" alt="warning sign"/>
+        </div>
+    )
+    
+}
