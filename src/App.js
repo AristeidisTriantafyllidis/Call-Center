e77de23825fefa034ae3240a21e7beaa2e373bd6
Aristeidis Triantafyllidis <tria.aris@gmail.com>
2026-05-13 17:05:41 +0300

    trying to set main page

--- src/App.js
+++ src/App.js
@@ -4,69 +4,87 @@ import Header from './Components/Header';
 import Data from './Components/Data';
 import MainCard from './Components/MainCard';
 import DetailCard from './Components/DetailCard';
+import LoadingPage from './LoadingErrors/LoadingPage';
 
 function App() {
+ const [allCalls, setAllCalls] = React.useState(null);
+ const[loading,setLoading]=React.useState(true)
+React.useEffect(() => {
+   fetch("https://call-center-mu.vercel.app/calls", {
+      headers: {
+         "X-User-Id": "Aris"
+      }
+   })
+   .then(res => res.json())
+   .then(data => {
+      setAllCalls(data);
+      setLoading(false)
+       })
+      
+
+},[])
+
 
   const [display, setDisplay] = React.useState(true)
-  const [allCalls, setAllCalls] = React.useState(Data);
-  const [selectedCall, setSelectedCall] = React.useState(allCalls[0])
+  const [selectedCall, setSelectedCall] = React.useState(allCalls)
  
   function toggleScreen(id) {
    setDisplay(!display)
-    const updateSelectedCall = allCalls.find((item) => item.id === id)
-    console.log(updateSelectedCall);
-    console.log(id)
-    setSelectedCall(updateSelectedCall)
+    // const updateSelectedCall = allCalls.find((item) => item.id === id)
+    
+    // setSelectedCall(updateSelectedCall)
   }
 
-  const Main = allCalls.map((mock) => {
-    const [date, hour] = mock.created_at.replace("Z", "").split("T");
-    return (
+  // const Main = allCalls.calls.map((mock) => {
+  //   const [date, hour] = mock.created_at.replace("Z", "").split("T");
+  //   return (
 
-      <MainCard
+  //     <MainCard
 
-        key={mock.id}
-        direction={mock.direction}
-        from={mock.from}
-        to={mock.to}
-        date={date}
-        hour={hour}
-        duration={mock.duration}
-        status={mock.call_type}
-        id={mock.id}
-        deleteCall={deleteCall}
-        switch={toggleScreen}
-      />
-    );
-  });
+  //       key={mock.id}
+  //       direction={mock.direction}
+  //       from={mock.from}
+  //       to={mock.to}
+  //       date={date}
+  //       hour={hour}
+  //       duration={mock.duration}
+  //       status={mock.call_type}
+  //       id={mock.id}
+  //       // deleteCall={deleteCall}
+  //       switch={toggleScreen}
+  //     />
+  //   );
+  // });
 
-  const Detail =
-    <DetailCard
-      key={selectedCall.id}
-      direction={selectedCall.direction}
-      id={selectedCall.id}
-      from={selectedCall.from}
-      to={selectedCall.to}
-      status={selectedCall.call_type}
-      duration={selectedCall.duration}
-      date={selectedCall.created_at}
-      archive={selectedCall.is_archived}
-      notes={selectedCall.notes ? selectedCall.notes[0].content : "No notes for this call"}
-      switch={toggleScreen}
-    />
+  // const Detail =
+  //   <DetailCard
+  //     key={selectedCall.id}
+  //     direction={selectedCall.direction}
+  //     id={selectedCall.id}
+  //     from={selectedCall.from}
+  //     to={selectedCall.to}
+  //     status={selectedCall.call_type}
+  //     duration={selectedCall.duration}
+  //     date={selectedCall.created_at}
+  //     archive={selectedCall.is_archived}
+  //     notes={selectedCall.notes ? selectedCall.notes[0].content : "No notes for this call"}
+  //     switch={toggleScreen}
+  //   />
 
 
-  function deleteCall(id) {
-    const updateCalls = allCalls.filter((item) => item.id !== id);
-    setAllCalls(updateCalls);
-  }
+  // function deleteCall(id) {
+  //   const updateCalls = allCalls.filter((item) => item.id !== id);
+  //   setAllCalls(updateCalls);
+  // }
 
+  console.log(allCalls)
 
   return (
     <div className="App">
       <Header />
-      {display ? <h3>Activity feed</h3> : ""}
-      {display ? Main : Detail}
+    {/* {display ? <h3>Activity feed</h3> : ""}  */}
+     {/* {display ? Main : Detail} */}
+     {loading?<LoadingPage/>:"Main"}
 
     </div>
   );
