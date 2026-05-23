ba52ba553118064dea4398cb5929d0f5c13d5d58
Aristeidis Triantafyllidis <tria.aris@gmail.com>
2026-05-23 16:25:37 +0300

    Update testing preparing deployment

--- src/App.test.js
+++ src/App.test.js
@@ -11,25 +11,25 @@ test('renders call center header', () => {
   expect(headerElement).toBeInTheDocument();
 });
 
-test('renders call type',() =>{
+test('renders call type', () => {
   render(<App />);
   const activity = screen.getByText(/call type/i);
   expect(activity).toBeInTheDocument();
 })
 
-test ('renders your page is loading',()=>{
+test('renders your page is loading', () => {
   render(<App />)
-  const loading=screen.getByText(/Your page is loading/i)
-expect(loading).toBeInTheDocument();
+  const loading = screen.getByText(/Your page is loading/i)
+  expect(loading).toBeInTheDocument();
 })
 
 
 
-test ('renders calls ', async ()=>{
-  global.fetch=jest.fn()
-  const mockData= {
-      calls:[
-        {
+test('renders calls ', async () => {
+  global.fetch = jest.fn()
+  const mockData = {
+    calls: [
+      {
         "id": "1",
         "direction": "inbound",
         "from": "+33 6 12 34 56 78",
@@ -38,13 +38,14 @@ test ('renders calls ', async ()=>{
         "duration": 120,
         "is_archived": false,
         "created_at": "2025-04-10T14:32:00Z"
-    }
-  ]};
-fetch.mockResolvedValue({
-  json:()=>Promise.resolve(mockData)
-})
+      }
+    ]
+  };
+  fetch.mockResolvedValue({
+    json: () => Promise.resolve(mockData)
+  })
   render(<App />)
-  await waitFor(()=>{
+  await waitFor(() => {
     expect(screen.getByText("Activity feed")).toBeInTheDocument();
     expect(screen.getByText("+33 6 12 34 56 78")).toBeInTheDocument();
   })
@@ -59,32 +60,33 @@ fetch.mockResolvedValue({
 //     jest.resetAllMocks()
 // })
 
-test("Renders calls tropos 2",async ()=>{
+test("Renders calls tropos 2", async () => {
 
 
-global.fetch.mockResolvedValueOnce({
-    json: async()=>({  calls:[
+  global.fetch.mockResolvedValueOnce({
+    json: async () => ({
+      calls: [
         {
-        "id": "1",
-        "direction": "inbound",
-        "from": "+33 6 12 34 56 78",
-        "to": "+33 1 23 45 67 89",
-        "call_type": "answered",
-        "duration": 120,
-        "is_archived": false,
-        "created_at": "2025-04-10T14:32:00Z"
-    }
-  ]
+          "id": "1",
+          "direction": "inbound",
+          "from": "+33 6 12 34 56 78",
+          "to": "+33 1 23 45 67 89",
+          "call_type": "answered",
+          "duration": 120,
+          "is_archived": false,
+          "created_at": "2025-04-10T14:32:00Z"
+        }
+      ]
 
     })
-})
-render(<App
-/>)
+  })
+  render(<App
+  />)
 
-await waitFor(()=> {
-expect(screen.getByText("+33 6 12 34 56 78")).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.getByText("+33 6 12 34 56 78")).toBeInTheDocument();
 
-})
+  })
 
 })
 
--- src/Components/MainCard/ MainCard.test.js
+++ src/Components/MainCard/ MainCard.test.js
@@ -5,27 +5,27 @@ import "@testing-library/jest-dom"
 import userEvent from "@testing-library/user-event";
 
 
-test("Find Button",()=>{
-render(<MainCard
-date="22/5/2026"
-/>)
-const Btn = screen.getByRole("button",{name:/Archive/i})
-expect(Btn).toBeInTheDocument();
+test("Find Button", () => {
+  render(<MainCard
+    date="22/5/2026"
+  />)
+  const Btn = screen.getByRole("button", { name: /Archive/i })
+  expect(Btn).toBeInTheDocument();
 })
 
-test("Click button",async () => {
-  const deleteCall=jest.fn()
-render (<MainCard
-date="22/5/2026"
-deleteCall={deleteCall}
+test("Click button", async () => {
+  const deleteCall = jest.fn()
+  render(<MainCard
+    date="22/5/2026"
+    deleteCall={deleteCall}
 
 
-/>)
-const Btn = screen.getByRole("button",{name:/Archive/i})
-await userEvent.click(Btn)
-expect(deleteCall).toHaveBeenCalledTimes(1)
-await userEvent.click(Btn)
-expect(deleteCall).toHaveBeenCalledTimes(2)
+  />)
+  const Btn = screen.getByRole("button", { name: /Archive/i })
+  await userEvent.click(Btn)
+  expect(deleteCall).toHaveBeenCalledTimes(1)
+  await userEvent.click(Btn)
+  expect(deleteCall).toHaveBeenCalledTimes(2)
 
 })
 
